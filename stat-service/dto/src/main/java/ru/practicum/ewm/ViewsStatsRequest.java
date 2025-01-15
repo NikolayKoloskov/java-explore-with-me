60e78cc323597eb7a55e4562da969529b4ec837c
Nikolay Koloskov <koloskovnick@yandex.ru>
2025-01-16 01:10:55 +0300

    Initial commit stat_svc - 3

--- stat-service/dto/src/main/java/ru/practicum/ewm/ViewsStatsRequest.java
+++ stat-service/dto/src/main/java/ru/practicum/ewm/ViewsStatsRequest.java
@@ -5,13 +5,12 @@ import lombok.Getter;
 import lombok.ToString;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Getter
 @ToString
 @Builder(toBuilder = true)
 public class ViewsStatsRequest {
-    private List<String> uris;
+    private String uris;
     @Builder.Default
     private LocalDateTime start = LocalDateTime.now().withHour(0).withMinute(0).withSecond(0);
     @Builder.Default
