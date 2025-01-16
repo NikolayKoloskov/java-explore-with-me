fabddb9da2f99c7d32ba3909e6aaf6692bdb73fe
Nikolay Koloskov <koloskovnick@yandex.ru>
2025-01-16 04:20:52 +0300

    Initial commit stat_svc - 9

--- stat-service/dto/src/main/java/ru/practicum/StatRequest.java
+++ stat-service/dto/src/main/java/ru/practicum/StatRequest.java
@@ -1,6 +1,9 @@
 package ru.practicum;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,9 +17,21 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class StatRequest {
     private int id;
+
+    @NotNull
+    @NotBlank
     private String app;
+
+    @NotBlank
     private String uri;
+
+    @NotNull
+    @NotBlank
+
     private String ip;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+
+    @NotNull
+    @JsonProperty("timestamp")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
 }
